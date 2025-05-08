5937fe9eccb5fecada6eb7d98ed8bc5cf3689779
asans2425 <asans@fpllefia.com>
2025-05-08 09:43:40 +0000

    Actualizar las URLs de imagen de los personajes en el seeder de PersonajeSeeder

--- database/seeders/PersonajeSeeder.php
+++ database/seeders/PersonajeSeeder.php
@@ -13,37 +13,37 @@ class PersonajeSeeder extends Seeder
         Personajes::insert([
             [
                 'nombre' => 'Mario',
-                'url_imagen' => 'https://static.wikia.nocookie.net/mario/images/3/3c/Wario_MP100.png/revision/latest/scale-to-width-down/1200?cb=20171120161202&path-prefix=es',
+                'url_imagen' => 'https://www.smashbros.com/assets_v2/img/fighter/wario/main8.png',
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now()
             ],
             [
                 'nombre' => 'Luigi',
-                'url_imagen' => 'https://static.wikia.nocookie.net/mario/images/3/3c/Wario_MP100.png/revision/latest/scale-to-width-down/1200?cb=20171120161202&path-prefix=es',
+                'url_imagen' => 'https://www.smashbros.com/assets_v2/img/fighter/wario/main8.png',
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now()
             ],
             [
                 'nombre' => 'Peach',
-                'url_imagen' => 'https://static.wikia.nocookie.net/mario/images/3/3c/Wario_MP100.png/revision/latest/scale-to-width-down/1200?cb=20171120161202&path-prefix=es',
+                'url_imagen' => 'https://www.smashbros.com/assets_v2/img/fighter/wario/main8.png',
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now()
             ],
             [
                 'nombre' => 'Bowser',
-                'url_imagen' => 'https://static.wikia.nocookie.net/mario/images/3/3c/Wario_MP100.png/revision/latest/scale-to-width-down/1200?cb=20171120161202&path-prefix=es',
+                'url_imagen' => 'https://www.smashbros.com/assets_v2/img/fighter/wario/main8.png',
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now()
             ],
             [
                 'nombre' => 'Toad',
-                'url_imagen' => 'https://static.wikia.nocookie.net/mario/images/3/3c/Wario_MP100.png/revision/latest/scale-to-width-down/1200?cb=20171120161202&path-prefix=es',
+                'url_imagen' => 'https://www.smashbros.com/assets_v2/img/fighter/wario/main8.png',
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now()
             ],
             [
                 'nombre' => 'Yoshi',
-                'url_imagen' => 'https://static.wikia.nocookie.net/mario/images/3/3c/Wario_MP100.png/revision/latest/scale-to-width-down/1200?cb=20171120161202&path-prefix=es',
+                'url_imagen' => 'https://www.smashbros.com/assets_v2/img/fighter/wario/main8.png',
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now()
             ],
